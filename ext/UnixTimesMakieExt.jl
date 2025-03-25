df7efba4d64b86fc98c77b64523b35c91dfe80fa
Ross Viljoen <ross.viljoen@graystonetrading.com>
2025-03-25 10:34:21 +0100

    custom epoch

--- ext/UnixTimesMakieExt.jl
+++ ext/UnixTimesMakieExt.jl
@@ -2,39 +2,46 @@ module UnixTimesMakieExt
 
 using UnixTimes
 using Makie
-using MakieCore
-using PlotUtils
+using Observables
 using Dates
 
-struct UnixTimeConversion <: Makie.AbstractDimConversion end
+struct UnixTimeConversion <: Makie.AbstractDimConversion
+    custom_epoch::Observable{UnixTime}
+    function UnixTimeConversion(custom_epoch=UnixTime(Date(2025, 01, 01)))
+        new(Observable{UnixTime}(custom_epoch; ignore_equal_values=true))
+    end
+end
+
+function number_to_unixtime(conversion::UnixTimeConversion, i)
+    Nanosecond(round(Int64, Float64(i))) + conversion.custom_epoch[]
+end
 
 Makie.needs_tick_update_observable(conversion::UnixTimeConversion) = nothing
 
-MakieCore.should_dim_convert(::Type{UnixTime}) = true
+Makie.MakieCore.should_dim_convert(::Type{UnixTime}) = true
 
 Makie.create_dim_conversion(::Type{UnixTime}) = UnixTimeConversion()
 
-Makie.convert_dim_value(conversion::UnixTimeConversion, value::UnixTime) = Dates.value(value)
-Makie.convert_dim_value(conversion::UnixTimeConversion, values::AbstractArray) = Dates.value.(values)
+function Makie.convert_dim_value(conversion::UnixTimeConversion, value::UnixTime)
+    Dates.value(value - conversion.custom_epoch[])
+end
+function Makie.convert_dim_value(conversion::UnixTimeConversion, values::AbstractArray{UnixTime})
+    Dates.value.(values .- conversion.custom_epoch[])
+end
 
 function Makie.convert_dim_observable(conversion::UnixTimeConversion, values::Observable, deregister)
-    result = map(values) do vs
-        Dates.value.(vs)
+    conversion.custom_epoch[] = min(conversion.custom_epoch[], first(values[]))
+    result = map(values, conversion.custom_epoch) do vs, ep
+        Dates.value.(vs .- ep)
     end
     append!(deregister, result.inputs)
     result
 end
 
 function Makie.get_ticks(conversion::UnixTimeConversion, ticks, scale, formatter, vmin, vmax)
-    # TODO: proper ticks for UnixTime
-    # Main.@infiltrate
-    # conversion, dates = PlotUtils.optimize_datetime_ticks(vmin, vmax; k_min=2, k_max=3)
     tickvalues = Makie.get_tickvalues(formatter, scale, vmin, vmax)
-    dates = number_to_unixtime.(round.(Int64, tickvalues))
-    Main.@infiltrate
+    dates = number_to_unixtime.(Ref(conversion), tickvalues)
     tickvalues, string.(dates)
 end
-
-number_to_unixtime(i) = UnixTime(Dates.UTInstant{Nanosecond}(Nanosecond(round(Int64, Float64(i)))))  # TODO: what type is i?
 
 end
