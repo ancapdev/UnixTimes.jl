b5cd88b57a0d5ab81f88bb3d7f518b29871ad71f
Ross Viljoen <ross+git@viljoen.co.uk>
2025-07-20 18:38:05 +0200

    Fix Makie@0.24 compat, and default epoch to UNIX_EPOCH

--- ext/UnixTimesMakieExt.jl
+++ ext/UnixTimesMakieExt.jl
@@ -6,15 +6,17 @@ using Observables
 using Dates
 
 struct UnixTimeConversion <: Makie.AbstractDimConversion
-    custom_epoch::Observable{Union{Nothing, UnixTime}}
-    function UnixTimeConversion(custom_epoch = nothing)
-        new(Observable{Union{Nothing, UnixTime}}(custom_epoch; ignore_equal_values=true))
+    custom_epoch::Observable{UnixTime}
+    function UnixTimeConversion(custom_epoch = UNIX_EPOCH)
+        new(Observable{UnixTime}(custom_epoch; ignore_equal_values=true))
     end
 end
 
 function number_to_unixtime(conversion::UnixTimeConversion, i)
-    Nanosecond(round(Int64, Float64(i))) + something(conversion.custom_epoch[], UNIX_EPOCH)
+    Nanosecond(round(Int64, Float64(i))) + conversion.custom_epoch[]
 end
+
+Makie.expand_dimensions(::PointBased, y::AbstractVector{<:UnixTime}) = (keys(y), y)
 
 Makie.needs_tick_update_observable(conversion::UnixTimeConversion) = nothing
 
@@ -23,19 +25,18 @@ Makie.should_dim_convert(::Type{UnixTime}) = true
 Makie.create_dim_conversion(::Type{UnixTime}) = UnixTimeConversion()
 
 function Makie.convert_dim_value(conversion::UnixTimeConversion, value::UnixTime)
-    Dates.value(value - something(conversion.custom_epoch[], UNIX_EPOCH))
+    Dates.value(value - conversion.custom_epoch[])
 end
 function Makie.convert_dim_value(conversion::UnixTimeConversion, values::AbstractArray{UnixTime})
-    Dates.value.(values .- something(conversion.custom_epoch[], UNIX_EPOCH))
+    Dates.value.(values .- conversion.custom_epoch[])
+end
+function Makie.convert_dim_value(conversion::UnixTimeConversion, attr, values, prev_values)
+    Dates.value.(values .- conversion.custom_epoch[])
 end
 
 function Makie.convert_dim_observable(conversion::UnixTimeConversion, values::Observable, deregister)
-    if conversion.custom_epoch[] === nothing
-        conversion.custom_epoch[] = last(values[])
-    end
-
     result = map(values, conversion.custom_epoch) do vs, ep
-        Dates.value.(vs .- something(ep, UNIX_EPOCH))
+        Dates.value.(vs .- ep)
     end
     append!(deregister, result.inputs)
     result
